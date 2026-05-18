e359928629c265b3ad45d5d06331dd56e510e142
kostyakopchan <kopchan.k@gmail.com>
2026-05-18 20:34:22 +0300

    adds ships preparation  module and draft for battle actions

--- stage_one/ships/ships.py
+++ stage_one/ships/ships.py
@@ -1,8 +1,8 @@
 import questionary
-from ships_config import SHIP_TYPES, SHIP_PARAMS, SHIP_SKILLS
+from .ships_config import SHIP_TYPES
 
 
-class Ship():
+class Ship:
     def __init__(self):
         self.name = input('Введите имя корабля: ')
         self.type = self.set_type()
@@ -12,16 +12,16 @@ class Ship():
         self.skill = self.set_skill()
 
     def set_type(self):
-        return questionary.select('Выберите тип корабля: ', choices=SHIP_TYPES).ask()
+        return questionary.select('Выберите тип корабля: ', choices=(list(SHIP_TYPES.keys()))).ask()
 
     def set_hp(self):
-        return SHIP_PARAMS[self.type]['hp']
+        return SHIP_TYPES[self.type]['hp']
 
     def set_speed(self):
-        return SHIP_PARAMS[self.type]['speed']
+        return SHIP_TYPES[self.type]['speed']
 
     def set_damage(self):
-        return SHIP_PARAMS[self.type]['damage']
+        return SHIP_TYPES[self.type]['damage']
 
     def set_skill(self):
-        return SHIP_SKILLS[self.type]
+        return SHIP_TYPES[self.type]['skill']
